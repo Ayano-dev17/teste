747e7c8f75f19dda2eb1d5b68920159e6950eccc
Ayano-dev17 <122577747+Ayano-dev17@users.noreply.github.com>
2023-05-29 15:56:30 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,35 +3,35 @@ var intervalo = 6000;
 
 function slide1(){
 
-    document.getElementById("banner").src="../img/sala01.jpeg";
+    document.getElementById("banner").src="sala01.jpeg";
     setTimeout("slide2()",intervalo);
 
 }
 
 function slide2(){
 
-    document.getElementById("banner").src="../img/sala02.jpeg";
+    document.getElementById("banner").src="sala02.jpeg";
     setTimeout("slide3()",intervalo);
 
 }
 
 function slide3(){
 
-    document.getElementById("banner").src="../img/sala03.jpeg";
+    document.getElementById("banner").src="sala03.jpeg";
     setTimeout("slide1()",intervalo);
 }
 
 
 function slide4(){
 
-    document.getElementById("banner").src="../img/sala04.jpeg";
+    document.getElementById("banner").src="sala04.jpeg";
     setTimeout("slide1()",intervalo);
 }
 
 
 function slide5(){
 
-    document.getElementById("banner").src="../img/sala05.jpeg";
+    document.getElementById("banner").src="sala05.jpeg";
     setTimeout("slide1()",intervalo);
 }
 
